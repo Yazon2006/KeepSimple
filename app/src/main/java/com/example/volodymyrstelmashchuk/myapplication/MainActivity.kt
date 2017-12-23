97a8ebd76b6e3492be364c8a6931b65846f754d6
Maksim Motorny (Kharkiv) <m.motorny@mobidev.biz>
2017-12-23 14:39:33 +0200

    screen component removed

--- app/src/main/java/com/example/volodymyrstelmashchuk/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/volodymyrstelmashchuk/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.volodymyrstelmashchuk.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    val mainPresenter = App.screenComponent.provideMainPresenter()
+    @Inject
+    lateinit var mainPresenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
